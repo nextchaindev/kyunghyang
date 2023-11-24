8067427cf1fd661664266825d13f1c19a972fc07
Van Tran <van.tran@nextchain.kr>
2023-11-24 09:50:53 +0700

    basic animation for 1st design-file and 360 viewer

--- scripts/common.util.js
+++ scripts/common.util.js
@@ -12,3 +12,16 @@ export const ColorUtil = {
     return brightness > 155;
   },
 };
+
+export const ElementUtil = {
+  isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  },
+};
