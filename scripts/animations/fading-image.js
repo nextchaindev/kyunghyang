e4062ff33612e3e4c553c5479f5668f29aa349da
Van Tran <van.tran@nextchain.kr>
2023-12-03 17:40:43 +0700

    update handle scroll anim for fade image and title text

--- scripts/animations/fading-image.js
+++ scripts/animations/fading-image.js
@@ -1,15 +1,17 @@
 // should be placed after gsap scripts
+// should be placed after gsap scripts
+const fadingImages = document.getElementsByClassName('fading-image');
 
-gsap.utils.toArray('.fading-image').forEach(function (elem) {
-  ScrollTrigger.create({
-    trigger: elem,
-    start: 'top 90%',
-    end: 'bottom 10%',
-    // markers: window.location.hostname === "127.0.0.1",
-    onEnter: function () {
+const snapIndexToShow = [1, 2];
+
+document.body.addEventListener('scroll', () => {
+  snapIndexToShow.forEach((snapIndex, imageIndex) => {
+    const top = snapIndex * innerHeight;
+
+    if (top === document.body.scrollTop) {
       setTimeout(() => {
         gsap.fromTo(
-          elem,
+          fadingImages[imageIndex],
           { opacity: 0, autoAlpha: 0 },
           {
             duration: 5,
@@ -19,11 +21,16 @@ gsap.utils.toArray('.fading-image').forEach(function (elem) {
             overwrite: 'auto',
           }
         );
-      }, 1000);
-    },
-    onLeave: function () {
+      }, 800);
+      return;
+    }
+
+    if (
+      top === document.body.scrollTop + innerHeight ||
+      top === document.body.scrollTop - innerHeight
+    ) {
       gsap.fromTo(
-        elem,
+        fadingImages[imageIndex],
         { opacity: 1, autoAlpha: 0 },
         {
           duration: 5,
@@ -33,34 +40,72 @@ gsap.utils.toArray('.fading-image').forEach(function (elem) {
           overwrite: 'auto',
         }
       );
-    },
-    onEnterBack: function () {
-      setTimeout(() => {
-        gsap.fromTo(
-          elem,
-          { opacity: 0, autoAlpha: 0 },
-          {
-            duration: 5,
-            opacity: 1,
-            autoAlpha: 1,
-            ease: 'back',
-            overwrite: 'auto',
-          }
-        );
-      }, 1000);
-    },
-    onLeaveBack: function () {
-      gsap.fromTo(
-        elem,
-        { opacity: 1, autoAlpha: 0 },
-        {
-          duration: 5,
-          opacity: 0,
-          autoAlpha: 1,
-          ease: 'back',
-          overwrite: 'auto',
-        }
-      );
-    },
+      return;
+    }
   });
 });
+
+// gsap.utils.toArray('.fading-image').forEach(function (elem) {
+//   ScrollTrigger.create({
+//     trigger: elem,
+//     start: 'top 90%',
+//     end: 'bottom 10%',
+//     // markers: window.location.hostname === "127.0.0.1",
+//     onEnter: function () {
+//       setTimeout(() => {
+//         gsap.fromTo(
+//           elem,
+//           { opacity: 0, autoAlpha: 0 },
+//           {
+//             duration: 5,
+//             opacity: 1,
+//             autoAlpha: 1,
+//             ease: 'back',
+//             overwrite: 'auto',
+//           }
+//         );
+//       }, 1000);
+//     },
+//     onLeave: function () {
+//       gsap.fromTo(
+//         elem,
+//         { opacity: 1, autoAlpha: 0 },
+//         {
+//           duration: 5,
+//           opacity: 0,
+//           autoAlpha: 1,
+//           ease: 'back',
+//           overwrite: 'auto',
+//         }
+//       );
+//     },
+//     onEnterBack: function () {
+//       setTimeout(() => {
+//         gsap.fromTo(
+//           elem,
+//           { opacity: 0, autoAlpha: 0 },
+//           {
+//             duration: 5,
+//             opacity: 1,
+//             autoAlpha: 1,
+//             ease: 'back',
+//             overwrite: 'auto',
+//           }
+//         );
+//       }, 1000);
+//     },
+//     onLeaveBack: function () {
+//       gsap.fromTo(
+//         elem,
+//         { opacity: 1, autoAlpha: 0 },
+//         {
+//           duration: 5,
+//           opacity: 0,
+//           autoAlpha: 1,
+//           ease: 'back',
+//           overwrite: 'auto',
+//         }
+//       );
+//     },
+//   });
+// });
--- scripts/animations/goingUp-text.js
+++ scripts/animations/goingUp-text.js
@@ -1,57 +1,101 @@
 // should be placed after gsap scripts
+const goingUpTexts = document.getElementsByClassName('goingUp-text');
 
-const goingUpTexts = gsap.utils.toArray(".goingUp-text");
+const snapIndexToShow = [
+  1, 2, 12, 12, 24, 24, 38, 38, 52, 52, 52, 52, 53, 53, 54, 54, 56, 56, 57, 58,
+  58,
+];
 
-goingUpTexts.forEach((elem) => {
-  ScrollTrigger.create({
-    trigger: elem,
-    start: "top 90%",
-    end: "bottom 10%",
-    // markers: window.location.hostname === "127.0.0.1",
-    onEnter() {
-      setTimeout(() => {
-        gsap.fromTo(
-          elem,
-          { y: 100, autoAlpha: 0 },
-          {
-            duration: 1.25,
-            y: 0,
-            autoAlpha: 1,
-            ease: "back",
-            overwrite: "auto",
-          }
-        );
-        gsap.fromTo(
-          elem,
-          { y: 100, autoAlpha: 0 },
-          {
-            duration: 1.25,
-            y: 0,
-            autoAlpha: 1,
-            ease: "back",
-            overwrite: "auto",
-          }
-        );
-      }, 250);
-    },
-    onLeave() {
-      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-    },
-    onEnterBack() {
+document.body.addEventListener('scroll', () => {
+  snapIndexToShow.forEach((snapIndex, textIndex) => {
+    let spacesOfNoneFullscreenSections = 0;
+
+    if (snapIndex > 12) {
+      const total =
+        document.getElementById('non1').offsetHeight +
+        document.getElementById('non2').offsetHeight;
+      spacesOfNoneFullscreenSections -= innerHeight * 2 - total;
+    }
+
+    const top = snapIndex * innerHeight + spacesOfNoneFullscreenSections;
+
+    textIndex === 4 &&
+      console.log(
+        snapIndex * innerHeight + spacesOfNoneFullscreenSections,
+        document.body.scrollTop
+      );
+
+    if (top === document.body.scrollTop) {
       gsap.fromTo(
-        elem,
-        { y: -100, autoAlpha: 0 },
+        goingUpTexts[textIndex],
+        { y: 100, opacity: 0, autoAlpha: 0 },
         {
           duration: 1.25,
           y: 0,
+          opacity: 1,
           autoAlpha: 1,
-          ease: "back",
-          overwrite: "auto",
+          ease: 'back',
+          overwrite: 'auto',
         }
       );
-    },
-    onLeaveBack() {
-      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
-    },
+      return;
+    }
+
+    if (
+      top === document.body.scrollTop + innerHeight ||
+      top === document.body.scrollTop - innerHeight
+    ) {
+      // alert('I am on');
+      gsap.fromTo(
+        goingUpTexts[textIndex],
+        { autoAlpha: 1 },
+        { autoAlpha: 0, overwrite: 'auto' }
+      );
+      return;
+    }
   });
 });
+
+// const goingUpTexts = gsap.utils.toArray('.goingUp-text');
+// goingUpTexts.forEach((elem) => {
+//   ScrollTrigger.create({
+//     trigger: elem,
+//     start: 'top 90%',
+//     end: 'bottom 10%',
+//     // markers: window.location.hostname === "127.0.0.1",
+//     onEnter() {
+//       setTimeout(() => {
+//         gsap.fromTo(
+//           elem,
+//           { y: 100, autoAlpha: 0 },
+//           {
+//             duration: 1.25,
+//             y: 0,
+//             autoAlpha: 1,
+//             ease: 'back',
+//             overwrite: 'auto',
+//           }
+//         );
+//       }, 250);
+//     },
+//     onLeave() {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: 'auto' });
+//     },
+//     onEnterBack() {
+//       gsap.fromTo(
+//         elem,
+//         { y: -100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: 'back',
+//           overwrite: 'auto',
+//         }
+//       );
+//     },
+//     onLeaveBack() {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: 'auto' });
+//     },
+//   });
+// });
